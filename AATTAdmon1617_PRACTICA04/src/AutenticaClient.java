3fd97fa53ecf78422c814692e885fd639e868f9e
Chupendo <andresruizpenuela@gmail.com>
2017-04-20 13:56:23 +0200

    Prueba git

--- AATTAdmon1617_PRACTICA04/src/AutenticaClient.java
+++ AATTAdmon1617_PRACTICA04/src/AutenticaClient.java
@@ -19,6 +19,8 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author Juan Carlos
+ * @author Andrés Ruiz Peñuela
+ * @author Luis Jesús
  */
 public class AutenticaClient extends javax.swing.JFrame {
 
